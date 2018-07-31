863f88226a9af026b1aff4a3d0ba0c17f926bbb0
Geoff Schmit <gcschmit@gmail.com>
2018-07-31 09:40:17 -0500

    removed use of current_app to use global app

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -1,7 +1,6 @@
 from threading import Thread
-from flask import current_app
 from flask_mail import Message
-from app import mail
+from app import app, mail
 
 def send_async_email(app, msg):
 	with app.app_context():
@@ -11,5 +10,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	Thread(target=send_async_email,
-			args=(current_app._get_current_object(), msg)).start()
+	Thread(target=send_async_email, args=(app, msg)).start()
