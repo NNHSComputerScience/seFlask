2f3f614d9ee1aef412c26e0ff82ffe0a3c4e8d16
Geoff Schmit <gcschmit@gmail.com>
2019-03-28 18:53:37 -0500

    incorrectly resolved the merge conflict

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -9,7 +9,6 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from config import Config
-from app.models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,14 +19,6 @@ login.login_view = 'auth.login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-@login.user_loader
-def load_user(id):
-	return User.query.get(int(id))
-
-def create_app(config_class=Config):
-	app = Flask(__name__)
-	app.config.from_object(config_class)
 
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
